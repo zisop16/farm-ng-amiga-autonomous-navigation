629c0b5c0cca6a7e7bbb090d7b16505796bd6674
zisop16 <zisop16@gmail.com>
2025-03-03 23:06:51 +0000

    created simple logging program for coordinates

--- gps_client/main.py
+++ gps_client/main.py
@@ -24,59 +24,15 @@ from farm_ng.core.events_file_reader import proto_from_json_file
 from farm_ng.gps import gps_pb2
 
 
-def print_relative_position_frame(msg):
-    """Prints the relative position frame message.
-
-    Args: msg: The relative position frame message.
-    """
-    print("RELATIVE POSITION FRAME \n")
-    print(f"Message stamp: {msg.stamp.stamp}")
-    print(f"GPS time: {msg.gps_time.stamp}")
-    print(f"Relative pose north: {msg.relative_pose_north}")
-    print(f"Relative pose east: {msg.relative_pose_east}")
-    print(f"Relative pose down: {msg.relative_pose_down}")
-    print(f"Relative pose length: {msg.relative_pose_length}")
-    print(f"Accuracy north: {msg.accuracy_north}")
-    print(f"Accuracy east: {msg.accuracy_east}")
-    print(f"Accuracy down: {msg.accuracy_down}")
-    print(f"Carrier solution: {msg.carr_soln}")
-    print(f"GNSS fix ok: {msg.gnss_fix_ok}")
-    print("-" * 50)
-
-
-def print_gps_frame(msg):
-    """Prints the gps frame message.
-
-    Args: msg: The gps frame message.
-    """
-    print("PVT FRAME \n")
-    print(f"Message stamp: {msg.stamp.stamp}")
-    print(f"GPS time: {msg.gps_time.stamp}")
-    print(f"Latitude: {msg.latitude}")
-    print(f"Longitude: {msg.longitude}")
-    print(f"Altitude: {msg.altitude}")
-    print(f"Ground speed: {msg.ground_speed}")
-    print(f"Speed accuracy: {msg.speed_accuracy}")
-    print(f"Horizontal accuracy: {msg.horizontal_accuracy}")
-    print(f"Vertical accuracy: {msg.vertical_accuracy}")
-    print(f"P DOP: {msg.p_dop}")
-    print("-" * 50)
-
-
 def print_ecef_frame(msg):
     """Prints the ecef frame message.
 
     Args: msg: The ecef frame message.
     """
-    print("ECEF FRAME \n")
-    print(f"Message stamp: {msg.stamp.stamp}")
+    # print("ECEF FRAME \n")
+    # print(f"Message stamp: {msg.stamp.stamp}")
     print(f"GPS time: {msg.gps_time.stamp}")
-    print(f"x: {msg.x}")
-    print(f"y: {msg.y}")
-    print(f"z: {msg.z}")
-    print(f"Accuracy: {msg.accuracy}")
-    print(f"Flags: {msg.flags}")
-    print("-" * 50)
+    print(f"Coordinates: ({(msg.x, msg.y, msg.z)})")
 
 
 async def main(service_config_path: Path) -> None:
@@ -87,13 +43,23 @@ async def main(service_config_path: Path) -> None:
     """
     # create a client to the camera service
     config: EventServiceConfig = proto_from_json_file(service_config_path, EventServiceConfig())
+    initial_time = None
+    time = 0
+    logging_duration = 10
+    coordinates = []
     async for event, msg in EventClient(config).subscribe(config.subscriptions[0]):
-        if isinstance(msg, gps_pb2.RelativePositionFrame):
-            print_relative_position_frame(msg)
-        elif isinstance(msg, gps_pb2.GpsFrame):
-            print_gps_frame(msg)
-        elif isinstance(msg, gps_pb2.EcefCoordinates):
-            print_ecef_frame(msg)
+        if isinstance(msg, gps_pb2.EcefCoordinates):
+            time = msg.gps_time.stamp
+            if initial_time is None:
+                initial_time = time
+            pos = (msg.x, msg.y, msg.z)
+            coordinates.append(str(pos))
+            if time > initial_time + logging_duration:
+                break
+    with open("coordinates.txt", 'w') as coordinates_file:
+        coordinates_file.write('\n'.join(coordinates))
+
+        
 
 
 if __name__ == "__main__":
