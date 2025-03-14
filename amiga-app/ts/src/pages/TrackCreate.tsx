835c67510a6b0d55eef695cb6ac518c7fc16c49c
zisop16 <zisop16@gmail.com>
2025-03-13 18:01:01 -0700

    added track create page

---
+++ amiga-app/ts/src/pages/TrackCreate.tsx
@@ -0,0 +1,10 @@
+import { Typography } from "@mui/material";
+import React from "react";
+
+export default function TrackCreate() {
+    return (
+        <>
+            <Typography variant="h2"></Typography>
+        </>
+    );
+}
