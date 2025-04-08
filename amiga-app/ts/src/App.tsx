34afe0ec3b641d1e2b8003f32b98e8de7030faaf
zisop16 <zisop16@gmail.com>
2025-04-07 18:56:30 -0700

    really fixed merge conflict

--- amiga-app/ts/src/App.tsx
+++ amiga-app/ts/src/App.tsx
@@ -3,10 +3,6 @@ import './App.css'
 import Home from './pages/Home'
 import TrackSelect from './pages/TrackSelect'
 import ViewCropYield from "./pages/ViewCropYield";
-<<<<<<< HEAD
-import TrackCreate from "./pages/TrackCreate";
-=======
->>>>>>> 2ffd193a1d4f6da38c883c3d7711c1e2ad66af85
 import { useEffect } from "react";
 
 function App() {
